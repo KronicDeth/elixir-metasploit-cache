0622ced6773978e1eb90ccb92c52a991b3fbcc83
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-31 23:37:00 -0500

    Add excoveralls as a dependency and test tool

--- mix.exs
+++ mix.exs
@@ -2,12 +2,15 @@ defmodule Metasploit.Cache.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :metasploit_cache,
+    [
+     app: :metasploit_cache,
      version: "0.0.1",
      elixir: "~> 1.0",
      elixirc_paths: ["lib", "web"],
      compilers: [:phoenix] ++ Mix.compilers,
-     deps: deps]
+     deps: deps,
+     test_coverage: [tool: ExCoveralls]
+    ]
   end
 
   # Configuration for the OTP application
@@ -22,7 +25,8 @@ defmodule Metasploit.Cache.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 0.10.0"},
+    [{:excoveralls, "~> 0.3.8", only: [:dev, :test]},
+     {:phoenix, "~> 0.10.0"},
      {:phoenix_ecto, "~> 0.1"},
      {:postgrex, ">= 0.0.0"},
      {:cowboy, "~> 1.0"}]
